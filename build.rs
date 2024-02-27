4e93b0d1ec0fdfab7d07ca4544a54daa3b2f9219
MeriKatt <devtanukikitamura@gmail.com>
2024-02-27 14:22:44 -0600

    add config.toml, revert build.rs changes

--- build.rs
+++ build.rs
@@ -1,11 +1,4 @@
 fn main() {
-    let mut oodle_path:String = "".to_string();
-    use std::path::Path;
-    if Path::new("C:/Program Files/Epic Games/UE_5.1/Engine/Source/Runtime/OodleDataCompression/Sdks/2.9.8/lib/Win64").exists() {
-        oodle_path = "C:/Program Files/Epic Games/UE_5.1/Engine/Source/Runtime/OodleDataCompression/Sdks/2.9.8/lib/Win64".to_string();
-    } else {
-        oodle_path = "E:/Unreal/UE_5.1/Engine/Source/Runtime/OodleDataCompression/Sdks/2.9.8/lib/Win64".to_string();
-    };
     winres::WindowsResource::new()
         .set_icon("src/assets/sybil.ico")
         .compile()
@@ -14,7 +7,7 @@ fn main() {
     println!(
         "cargo:rustc-link-search={}",
         std::env::var("OODLE").unwrap_or(
-            oodle_path
+            "C:/Program Files/Epic Games/UE_5.1/Engine/Source/Runtime/OodleDataCompression/Sdks/2.9.8/lib/Win64".to_string()
         )
     );
 }
